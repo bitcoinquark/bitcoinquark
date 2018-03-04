be87a2735e1584e4fb7bc7639d111b7102e493e5
Lorenzo <lorenzo@bitcoinquark.org>
2018-03-04 16:12:12 +0800

    test_framework

--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -99,9 +99,7 @@ class BitcoinTestFramework():
 
         PortSeed.n = self.options.port_seed
 
-        os.environ['PATH'] = self.options.srcdir + os.pathsep + \
-                             self.options.srcdir + os.path.sep + "qt" + os.pathsep + \
-                             os.environ['PATH']
+        os.environ['PATH'] = self.options.srcdir + ":" + self.options.srcdir + "/qt:" + os.environ['PATH']
 
         check_json_precision()
 
@@ -150,11 +148,10 @@ class BitcoinTestFramework():
             self.log.info("Note: bitcoinquarkds were not stopped and may still be running")
 
         if not self.options.nocleanup and not self.options.noshutdown and success != TestStatus.FAILED:
-            self.log.info("Cleaning up {} on exit".format(self.options.tmpdir))
-            cleanup_tree_on_exit = True
+            self.log.info("Cleaning up")
+            shutil.rmtree(self.options.tmpdir)
         else:
             self.log.warning("Not cleaning up dir %s" % self.options.tmpdir)
-            cleanup_tree_on_exit = False
 
         if success == TestStatus.PASSED:
             self.log.info("Tests successful")
@@ -167,8 +164,6 @@ class BitcoinTestFramework():
             self.log.error("Hint: Call {} '{}' to consolidate all logs".format(os.path.normpath(os.path.dirname(os.path.realpath(__file__)) + "/../combine_logs.py"), self.options.tmpdir))
             exit_code = TEST_EXIT_FAILED
         logging.shutdown()
-        if cleanup_tree_on_exit:
-            shutil.rmtree(self.options.tmpdir)
         sys.exit(exit_code)
 
     # Methods to override in subclass test scripts.
@@ -392,7 +387,7 @@ class BitcoinTestFramework():
             # Create cache directories, run bitcoinquarkds:
             for i in range(MAX_NODES):
                 datadir = initialize_datadir(self.options.cachedir, i)
-                args = [os.getenv("BITCOIND", "bitcoinquarkd"), "-datadir=" + datadir]
+                args = [os.getenv("BITCOIND", "bitcoinquarkd"), "-server", "-keypool=1", "-datadir=" + datadir, "-discover=0"]
                 if i > 0:
                     args.append("-connect=127.0.0.1:" + str(p2p_port(0)))
                 self.nodes.append(TestNode(i, self.options.cachedir, extra_args=[], rpchost=None, timewait=None, binary=None, stderr=None, mocktime=self.mocktime, coverage_dir=None))
